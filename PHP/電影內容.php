a071eaad5f61fb104352a129fa9a2f713453ba75
sisia881112 <D0746490@o365.fcu.edu.tw>
2020-06-05 14:10:09 +0800

    電影內容頁面

--- PHP/電影內容.php
+++ PHP/電影內容.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>登入</title>
+    <title>推薦</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
@@ -12,11 +12,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="../CSS/css.css" />
-
+    <link rel="stylesheet" type="text/css" href="../CSS/電影內容.css" />
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark">
+<nav class="navbar navbar-expand-lg navbar-dark">
         <a href="#" class="navbar-brand">Projectxx2020</a>
         <button class="navbar-toggler" type="button" data-target="#navigation">
             <span class="navbar-toggler-icon"></span>
@@ -24,7 +24,7 @@
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a href="首頁.html" class="nav-link">Home |</a>
+                    <a href="首頁.html" class="nav-link">Home </a>
                 </li>
                 <li class="nav-item">
                     <a href="登入.html" class="nav-link">登入 |</a>
@@ -33,11 +33,34 @@
                     <a href="搜尋.html" class="nav-link">搜尋 |</a>
                 </li>
                 <li class="nav-item">
-                    <a href="../PHP/推薦.php" class="nav-link">推薦 |</a>
+                    <a href="推薦.html" class="nav-link">推薦 |</a>
                 </li>
             </ul>
         </div>
     </nav>
+<div class="container">
+   <div class="row">
+      <div class="col">
+         <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+               <strong class="d-inline-block mb-2 text-primary">MovieTitle</strong>
+               <h6 class="mb-0">
+                  <a class="text-dark" href="#">Cname</a>
+               </h6>
+               <h6 class="mb-0">
+                  <a class="text-dark" href="#">Dname</a>
+               </h6>
+               <div class="mb-1 text-muted small">Classification</div>
+               <p class="card-text mb-auto">Introduction</p>
+               <a class="btn btn-outline-primary btn-sm" role="button" href="">Watch Now</a>
+            </div>
+            <img class="card-img-right flex-auto d-none d-lg-block" alt="還沒放" src="" style="width: 200px; height: 250px;">
+         </div>
+      </div>
+</div>
+
+    
+
 </body>
 
 </html>
